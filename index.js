9f6af4d0895b115d7e3f5dd5cf4a4e5d6fc8e493
Mike K <mkimweb@outlook.com>
2024-07-22 13:04:56 +0200

    Added logger using Winston

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import booksRouter from "./routes/books.js";
 import recordsRouter from "./routes/records.js";
+import log from "./middleware/logMiddleware.js";
+import logger from "./utils/log.js";
 
 const app = express();
 app.use(express.json());
+
+app.use(log);
 
 app.use("/books", booksRouter);
 app.use("/records", recordsRouter);
@@ -12,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  logger.info("Server is listening on port 3000");
 });
