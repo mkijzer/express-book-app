23b066bd606921f26abd94965b3507c338e42f11
Mike K <mkimweb@outlook.com>
2024-07-24 10:07:23 +0200

    Add login route with JWT authentication

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import booksRouter from "./routes/books.js";
 import recordsRouter from "./routes/records.js";
+import loginRouter from "./routes/login.js";
 import log from "./middleware/logMiddleware.js";
-import logger from "./utils/log.js";
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
@@ -11,12 +12,12 @@ app.use(log);
 
 app.use("/books", booksRouter);
 app.use("/records", recordsRouter);
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  logger.info("Server is listening on port 3000");
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
